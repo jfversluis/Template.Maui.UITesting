da58a5795de5b3f5670e5faed980c7dce53b0ffa
Gerald Versluis <gerald.versluis@microsoft.com>
2024-05-30 21:24:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Template.Maui.UITesting
+
+Currently under development, a set of templates that will make it easier to add UI test projects to your .NET MAUI solution.
+
+You think this is useful? Let me know!
---
+++ content/xUnit-Appium-CSharp/UITests.Shared/BaseTest.cs
@@ -0,0 +1,32 @@
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using Xunit;
+
+namespace UITests;
+
+// Add a CollectionDefinition together with a ICollectionFixture
+// to ensure that the setup only runs once
+// xUnit does not have a built-in concept of a fixture that only runs once for the whole test set.
+[CollectionDefinition("UITests")]
+public sealed class UITestsCollectionDefinition : ICollectionFixture<AppiumSetup>
+{
+
+} 
+
+// Add all tests to the same collection as above so that the Appium server is only setup once
+[Collection("UITests")]
+public abstract class BaseTest
+{
+	protected AppiumDriver App => AppiumSetup.App;
+
+	// This could also be an extension method to AppiumDriver if you prefer
+	protected AppiumElement FindUIElement(string id)
+	{
+		if (App is WindowsDriver)
+		{
+			return App.FindElement(MobileBy.AccessibilityId(id));
+		}
+
+		return App.FindElement(MobileBy.Id(id));
+	}
+}
