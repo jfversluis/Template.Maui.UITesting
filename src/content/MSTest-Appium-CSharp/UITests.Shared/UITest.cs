aa40782afcee51ce76369e9e6cfb80fcde69d4f1
Gerald Versluis <gerald.versluis@microsoft.com>
2024-05-31 14:04:44 +0200

    Add MSTest

---
+++ src/content/MSTest-Appium-CSharp/UITests.Shared/UITest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// You will have to make sure that all the namespaces match
+// between the different platform specific projects and the shared
+// code files. This has to do with how we initialize the AppiumDriver
+// through the AppiumSetup.cs files and MSTest AssemblyInitialize attributes.
+// Also see: https://learn.microsoft.com/dotnet/core/testing/unit-testing-with-mstest
+namespace UITests;
+
+// This is an example of tests that do not need anything platform specific.
+// Typically you will want all your tests to be in the shared project so they are ran across all platforms.
+[TestClass]
+public class MainPageTests : BaseTest
+{
+	[TestMethod]
+	public void AppLaunches()
+	{
+		App.GetScreenshot().SaveAsFile($"{nameof(AppLaunches)}.png");
+	}
+}
---
+++ src/content/MSTest-Appium-CSharp/UITests.Windows/AppiumSetup.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace UITests;
+
+[TestClass]
+public class AppiumSetup
+{
+	private static AppiumDriver? driver;
+
+	public static AppiumDriver App => driver ?? throw new NullReferenceException("AppiumDriver is null");
+
+	[AssemblyInitialize]
+	public void RunBeforeAnyTests()
+	{
+#if (includeAppiumServerStartup)
+		// If you started an Appium server manually, make sure to comment out the next line
+		// This line starts a local Appium server for you as part of the test run
+		AppiumServerHelper.StartAppiumLocalServer();
+
+#endif
+		var windowsOptions = new AppiumOptions
+		{
+			// Specify windows as the driver, typically don't need to change this
+			AutomationName = "windows",
+			// Always Windows for Windows
+			PlatformName = "Windows",
+			// The identifier of the deployed application to test
+			App = "com.companyname.basicappiumsample_9zz4h110yvjzm!App",
+		};
+
+		// Note there are many more options that you can use to influence the app under test according to your needs
+
+		driver = new WindowsDriver(windowsOptions);
+	}
+
+	[AssemblyCleanup]
+	public void RunAfterAnyTests()
+	{
+		driver?.Quit();
+#if (includeAppiumServerStartup)
+
+		// If an Appium server was started locally above, make sure we clean it up here
+		AppiumServerHelper.DisposeAppiumLocalServer();
+#endif
+	}
+}
